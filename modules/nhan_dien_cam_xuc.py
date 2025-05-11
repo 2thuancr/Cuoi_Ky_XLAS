b7a84c9bef9f4915637aca5052029e48e969ece2
2ThuanCr <2thuancr03@gmail.com>
2025-05-11 20:13:30 +0700

    old

--- modules/nhan_dien_cam_xuc.py
+++ modules/nhan_dien_cam_xuc.py
@@ -7,11 +7,11 @@ import torchvision.transforms as transforms
 
 
 # Nhãn cảm xúc (phải đúng thứ tự)
-class_names = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'sleepy', 'surprise']
-# class_names = ['Happy', 'Sleepy', 'Surprise']
+# class_names = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'sleepy', 'surprise']
+class_names = ['Happy', 'Sleepy', 'Surprise']
 
 # Load ONNX model
-session = ort.InferenceSession("./model/emotion_cnn.onnx", providers=['CPUExecutionProvider'])
+session = ort.InferenceSession("./model/emotion_cnn_old.onnx", providers=['CPUExecutionProvider'])
 
 # Hàm tiền xử lý
 def preprocess_image(img: Image.Image):
